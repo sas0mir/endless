5e3e29d5341af260ff49fbf5d421b9253e074c04
sas0mir <sas_mir@icloud.com>
2025-04-21 23:47:11 +0200

    I_003: moving to Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr: true,
+  app: {
+    baseURL: '/',
+    buildAssetsDir: '/_nuxt'
+  },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ]
+})
